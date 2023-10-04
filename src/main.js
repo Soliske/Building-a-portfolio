4527d75c1291439655aa85a3d52a9512b54625ae
Kevin Solis <soliskayvon@gmail.com>
2023-10-04 09:11:30 -0700

    troubleshoot files

--- src/main.js
+++ src/main.js
@@ -4,7 +4,3 @@ import '../styles/components/header.css';
 import '../styles/components/hero.css';
 import '../styles/components/about.css';
 import '../styles/utils.css';
-
-import mobileNav from './assets/index-d285d195.js';
-
-mobileNav();
