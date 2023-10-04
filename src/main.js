faf3f409ff4500fb98dd07dddec878d1c7fe0a36
Kevin Solis <soliskayvon@gmail.com>
2023-10-04 09:02:05 -0700

    changes to build. Linked CSS in HTML

--- src/main.js
+++ src/main.js
@@ -4,3 +4,7 @@ import '../styles/components/header.css';
 import '../styles/components/hero.css';
 import '../styles/components/about.css';
 import '../styles/utils.css';
+
+import mobileNav from './assets/index-d285d195.js';
+
+mobileNav();
